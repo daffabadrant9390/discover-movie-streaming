53eeea5adf12e6586c0a56e4ef8a907778b3523a
daffabadrant9390 <daffabadrant@gmail.com>
2023-04-18 23:40:43 +0700

    feat: initial commit - import google font to _app.js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Roboto_Slab, Playfair_Display } from 'next/font/google';
+
+const robotoSlab = Roboto_Slab({
+  subsets: ['latin'],
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={robotoSlab.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
 
-export default MyApp
+export default MyApp;
